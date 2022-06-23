11ec851c5539e7b7e9ef20d9d2f1da783577891d
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2022-06-24 04:56:04 +0800

    Add icons for links to socials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Jonathan Wong</h1>
         <h5 className="text-light">Software Engineer</h5>
         <CTA/>
+        <HeaderSocials/>
         
         <div className="me">
           <img src={ME} alt="" />
