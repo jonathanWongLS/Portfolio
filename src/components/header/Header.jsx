a3aa906cbf8ddd32805f821319c81fb40c85e0f3
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2022-06-24 04:44:33 +0800

    Created header with links to download resume and to redirect to contacts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Jonathan Wong</h1>
+        <h5 className="text-light">Software Engineer</h5>
+        <CTA/>
+        
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        
+    </div>
+  )
+} 
