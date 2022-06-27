6b5d8dcd5133e777d41380733f55b400da225283
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2022-06-28 02:54:17 +0800

    Implemented functionality where users can send message straight to my email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,9 +3,25 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
-
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_alcpdlq', 'template_hlrba3z', form.current, '1GYOds4BwEAS2oMbb')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+  };
+  
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -15,21 +31,21 @@ const Contact = () => {
         <div className="contact__options">
 
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>wonglsjonathan01@gmail</h5>
             <a href="mailto:wonglsjonathan01@gmail.com">Send a message</a>
           </article>
 
           <article className="contact__option">
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>(+60)017-380-9738</h5>
             <a href="https://api.whatsapp.com/send?phone=600173809738" target='_blank' rel='noreferrer'>Send a message</a>
           </article>
 
           <article className="contact__option">
-            <RiMessengerLine/>
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>Jonathan Wong</h5>
             <a href="https://m.me/jonathan.wong.01" target='_blank' rel='noreferrer'>Send a message</a>
@@ -37,10 +53,10 @@ const Contact = () => {
         </div>
         {/* END OF CONTACT OPTIONS */}
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required/>
-          <input type="message" rows="7" placeholder='Your Message' required />
+          <input type="message" name='message' rows="7" placeholder='Your Message' required />
           <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
 
