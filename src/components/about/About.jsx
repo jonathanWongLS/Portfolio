aad8920f95f7bf5a718a641094a3c2485fdd3624
JonWLS <jwon0100@student.monash.edu>
2022-10-01 17:03:45 +0800

    Updated "About" section text format for improved readability

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import './about.css'
 import ME from '../../assets/passport_photo.png'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
     return (
@@ -14,35 +14,37 @@ const About = () => {
             <div className="container about__container">
                 <div className="about__me">
                     <div className="about__me-image">
-                        <img src={ME} alt="About Image" />
+                        <img src={ME} alt="About Me Icon" />
                     </div>
                 </div>
 
                 <div className="about__content">
                     <div className="about__cards">
-                        
+
                         <article className="about__card">
-                            <FaAward className="about__icon"/>
+                            <FaAward className="about__icon" />
                             <h5>Experience</h5>
                             <small>3rd Year Bachelor of Software Engineering (Honours) Student</small>
                         </article>
 
                         <article className="about__card">
-                            <FiUsers className="about__icon"/>
+                            <FiUsers className="about__icon" />
                             <h5>Clients</h5>
                             <small>0</small>
                         </article>
 
                         <article className="about__card">
-                            <VscFolderLibrary className="about__icon"/>
+                            <VscFolderLibrary className="about__icon" />
                             <h5>Projects</h5>
-                            <small>4 Projects</small>
+                            <small>3 Projects</small>
                         </article>
                     </div>
                     <p>
-                        Highly motivated and inspired to continuously sharpen my skills and grow professionally in data science and other data related skills such as machine learning and data engineering. 
-                        Always ready to develop my programming and soft skills in internship training and online courses, working with other like-minded individuals in inspiring the world on how influential Big Data is in developing accurate theories and putting into place more effective solutions for countless problems globally. 
-                        Extremely keen on working in teams, collaborating and interacting with team members in brainstorming creative and innovative solutions, thus encouraging the birth of practical and profitable ideas.
+                        Penultimate undergraduate pursuing a Bachelor in Software Engineering (Honours) seeking a position as an intern in the front-end development field. 
+                        <br/><br/>Around 2 years of experience in designing and implementing user interactive websites with HTML, CSS and JavaScript (ReactJS). 
+                        <br/><br/>Highly motivated and inspired to continuously sharpen my skills and grow professionally in front-end Developing and other Web Development related skills such as Backend Development and UI/UX.
+                        <br/><br/>Always ready to develop my programming and soft skills in internship training and projects.
+                        <br/><br/>Extremely keen on working effectively in teams, collaborating and interacting with team members in brainstorming creative and innovative solutions, thus encouraging the birth of practical and profitable ideas.
                     </p>
 
                     <a href="#contact" className="btn btn-primary">
