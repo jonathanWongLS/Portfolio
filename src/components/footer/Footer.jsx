2130d353120418835a993dfbbe5f397745372687
JonWLS <jwon0100@student.monash.edu>
2022-10-01 18:30:17 +0800

    Include GitHub, LinkedIn and Instagram link to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './footer.css'
-import {FaFacebookF} from 'react-icons/fa'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 import {FiInstagram} from 'react-icons/fi'
-import {IoLogoTwitter} from 'react-icons/io'
 
 const Footer = () => {
   return (
@@ -20,9 +20,9 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-        <a href="https://facebook.com"><FaFacebookF/></a>
-        <a href="https://instgram.com"><FiInstagram/></a>
-        <a href="https://twitter.com"><IoLogoTwitter/></a>
+        <a href="https://www.linkedin.com/in/jonathan-wong-123372130/" target="_blank" rel='noreferrer'><BsLinkedin/></a>
+        <a href="https://github.com/jonathanWongLS" target="_blank" rel='noreferrer'><FaGithub/></a>
+        <a href="https://www.instagram.com/jon_w01/"><FiInstagram/></a>
       </div>
 
       <div className="footer__copyright">
