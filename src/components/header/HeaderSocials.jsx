90fd3221984fc2b6fea92a4838eb32560338441d
JonWLS <jwon0100@student.monash.edu>
2022-10-01 17:55:57 +0800

    Add rel=noreferrer to protect referral information from being passed to the target website and to hide referral traffic in Google analytics.

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,9 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/jonathan-wong-123372130/" target="_blank" ><BsLinkedin/></a>
-      <a href="https://github.com/jonathanWongLS" target="_blank"><FaGithub/></a>
-      
+      <a href="https://www.linkedin.com/in/jonathan-wong-123372130/" target="_blank" rel='noreferrer'><BsLinkedin/></a>
+      <a href="https://github.com/jonathanWongLS" target="_blank" rel='noreferrer'><FaGithub/></a>
     </div>
   )
 } 
