0a4fcc7893500639fce1b0a950167291d5a4b26e
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-05-23 04:26:53 +0800

    Show Services section and update web development services offered

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -9,110 +9,67 @@ const Services = () => {
       <h2>Services</h2>
 
       <div className="container services__container">
+        <div></div>
+
         <article className="service">
           <div className="service__head">
             <h3>Web Development</h3>
           </div>
-
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>Develop complete web applications with both front-end and back-end components.</p>
             </li>
             
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>Ensure seamless integration between front-end and back-end systems.</p>
             </li>
 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>Design and implement user interfaces using HTML5, CSS3, JavaScript, and ReactJS.</p>
             </li>
 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>Create responsive and interactive web pages.</p>
             </li>
 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>Develop and maintain server-side logic with Java Spring Boot.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Implement secure APIs and authentication systems (e.g., JWT).</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Design intuitive and user-friendly interfaces.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Utilize AWS EC2 for hosting back-end servers.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Manage databases with AWS RDS and deploy static websites on AWS S3.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Containerize applications using Docker.</p>
             </li>
 
           </ul>
         </article>
         {/*END OF WEB DEVELOPMENT*/}
-        <article className="service">
-          <div className="service__head">
-            <h3>Data Science</h3>
-          </div>
-
-          <ul className='service__list'>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-            
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-
-          </ul>
-          
-        </article>
-        {/*END OF DATA SCIENCE*/}
-        
-        <article className="service">
-          <div className="service__head">
-            <h3>UI/UX Design</h3>
-          </div>
-
-          <ul className='service__list'>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-            
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </li>
-
-          </ul>
-
-        </article>
+        <div></div>
       </div>
     </section>
   )
