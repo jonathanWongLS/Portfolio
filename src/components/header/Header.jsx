3eeaa255504d464457ad21c3cb9313cae5affb86
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-05-23 04:29:03 +0800

    Updated my career name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,14 +10,12 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Jonathan Wong</h1>
-        <h5 className="text-light">Software Engineer</h5>
+        <h5 className="text-light">Software Engineer | Front-end Developer</h5>
         <CTA/>
         <HeaderSocials/>
-        
         <div className="me">
           <img src={ME} alt="" />
         </div>
-
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
